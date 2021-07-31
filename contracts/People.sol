ad1e4ca321f1f1f76b441e0fefecffe7d4fcf145
formjune <formjune@gmail.com>
2021-07-31 23:42:46 +0500

    fill
    fill

--- contracts/People.sol
+++ contracts/People.sol
@@ -1,32 +1,61 @@
-// Specifies the version of Solidity, using semantic versioning.
-// Learn more: https://solidity.readthedocs.io/en/v0.5.10/layout-of-source-files.html#pragma
-pragma solidity ^0.5.2;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
 
-// Defines a contract named `HelloWorld`.
-// A contract is a collection of functions and data (its state).
-// Once deployed, a contract resides at a specific address on the Ethereum blockchain.
-// Learn more: https://solidity.readthedocs.io/en/v0.5.10/structure-of-a-contract.html
-contract HelloWorld {
+import "./ERC721.sol";
 
-    // Declares a state variable `message` of type `string`.
-    // State variables are variables whose values are permanently stored in contract storage.
-    // The keyword `public` makes variables accessible from outside a contract
-    // and creates a function that other contracts or clients can call to access the value.
-    string public message;
+contract People is ERC721, Ownable{
 
-    // Similar to many class-based object-oriented languages, a constructor is
-    // a special function that is only executed upon contract creation.
-    // Constructors are used to initialize the contract's data.
-    // Learn more: https://solidity.readthedocs.io/en/v0.5.10/contracts.html#constructors
-    constructor(string memory initMessage) public {
-        // Accepts a string argument `initMessage` and sets the value
-        // into the contract's `message` storage variable).
-        message = initMessage;
+    uint constant GIFT_MAX = 66;
+    uint constant SELL_MAX = 6666;
+    uint constant MULTI_SELL_MAX = SELL_MAX - 50;
+    uint constant MULTI_SELL = 10;
+    uint constant PRICE = .05 ether;
+    address[] RECEIVERS = [0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c, 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db];
+    uint[] PERCENTAGE = [30, 70];
+
+    uint public sold = GIFT_MAX;
+    uint public gifted = 0;
+    bool public multi_mint = true;
+    bool public sold_out = false;
+    
+    constructor() ERC721("People", "People"){}
+
+    function setBaseURI(string memory _uri) public onlyOwner{
+        _setBaseURI(_uri);
     }
 
-    // A public function that accepts a string argument
-    // and updates the `message` storage variable.
-    function update(string memory newMessage) public {
-        message = newMessage;
+    function withdraw() private{
+        // automatically send ether to receivers
+        for (uint i = 0; i < RECEIVERS.length; i++)
+            require(payable(RECEIVERS[i]).send(msg.value * PERCENTAGE[i] / 100), "transfer error");
+    }
+
+    function sendGift(address _to) public onlyOwner{
+        // send a gift
+        require(gifted < GIFT_MAX, "no more gifts");
+        _safeMint(_to, ++gifted);
+    }
+
+    function mint(uint _count) public payable{
+        // mint pack (10 max)
+        require(!sold_out, "sold out");
+        require(multi_mint, "multi mint is no longer available");
+        require(_count <= MULTI_SELL, "can't mint more than 10 at once");
+        require(msg.value >= _count * PRICE, "not enough money");
+        for (uint i = 0; i < _count; i++)  
+            _safeMint(msg.sender, ++sold);
+        withdraw();
+        if(sold >= MULTI_SELL_MAX)
+            multi_mint = false;
+    }
+
+    function mint() public payable{
+        // mint one (at the end of sale)
+        require(!sold_out, "sold out");
+        require(msg.value >= PRICE, "not enough money");
+        _safeMint(msg.sender, ++sold);
+        withdraw();
+        if(sold == SELL_MAX)
+            sold_out = true;
     }
 }
