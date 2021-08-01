046b42c3bbf16aa2780461f49ab5429ff7d06068
formjune <formjune@gmail.com>
2021-08-01 18:07:35 +0500

    b
    b

--- contracts/RebelKids.sol
+++ contracts/RebelKids.sol
@@ -7,56 +7,90 @@ import "./ERC721.sol";
 
 contract RebelKids is ERC721, Ownable{
 
+    // structures and enums
+    struct Recipient{
+        address payable recipient;
+        uint percentage;
+    }
+
+    // events
+    event RecipientAdded(
+        address payable recipient,
+        uint percentage
+    );
+
+    event PurchaseMade(
+        address payable customer,
+        uint amount
+    );
+
+    event GiftSent(
+        address customer
+    );
+
+    event SaleStarted();
+    event SalePaused();
+
+    // constants
     uint constant GIFT_MAX = 66;
     uint constant SELL_MAX = 6666;
     uint constant MULTI_SELL = 10;
     uint constant PRICE = .05 ether;
-    enum SALE_STATES{NOT_STARTED, ACTIVE, PAUSED, FINISHED}
-    address[] private RECEIVERS = [0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c, 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db];
-    uint[] private PERCENTAGE = [30, 70];
-
+    
+    // variables
     uint private gifted = 0;
-    SALE_STATES public sale_state = SALE_STATES.NOT_STARTED;
+    Recipient[] private recipients = [];
+    bool sale_active = false;
 
-    constructor() ERC721("People", "People"){
+    constructor() ERC721("Rebel Kids", "Rebel Kids"){
+
+        // reserve 66 tokens
         for(uint i = 1; i <= GIFT_MAX; i++)
             _safeMint(msg.sender, i);
     }
+
+    // owner interface
 
     function setBaseURI(string memory _uri) public onlyOwner{
         _setBaseURI(_uri);
     }
 
+    function addRecipient(address payable _address, uint _percentage) public onlyOwner{
+        recipients.push(Recipient(_address, _percentage));
+        emit RecipientAdded(_address, _percentage);
+    }
+
+    function withdraw() public onlyOwner{
+        for (uint i = 0; i < recipients.length; i++)
+            require(recipients[i].recipient.send(address(this).balance * recipients[i].percentage / 100), "transfer error");
+    }
+
     function startSale() public onlyOwner{
-        require(sale_state != SALE_STATES.FINiSHED, "sale is finished");
-        sale_state = SALE_STATES.ACTIVE;
+        sale_active = true;
+        emit SaleStarted();
     }
 
     function pauseSale() public onlyOwner{
-        require(sale_state != SALE_STATES.FINiSHED, "sale is finished");
-        sale_state = SALE_STATES.PAUSED;
-    }
-
-    function withdraw() private{
-        for (uint i = 0; i < RECEIVERS.length; i++)
-            require(payable(RECEIVERS[i]).send(address(this).balance * PERCENTAGE[i] / 100), "transfer error");
+        sale_active = false;
+        emit SalePaused();
     }
 
     function sendGift(address _to) public onlyOwner{
         require(gifted < GIFT_MAX, "no more gifts");
         _safeMint(_to, ++gifted);
+        emit GiftSent(_to);
     }
 
-    function mint(uint _count) public payable{
-        require(sale_state != SALE_STATES.NOT_STARTED, "embrace patience and return whence thou hast cometh");
-        require(sale_state != SALE_STATES.FINISHED, "sold out");
-        require(sale_state != SALE_STATES.PAUSED, "sale is paused");
-        require(0 < _count <= MULTI_SELL, "number of tokens must be between 1 and 10");
-        require(msg.value >= _count * PRICE, "not enough money");
-        require(totalSupply() + _count <= SELL_MAX, "not enough tokens for sale");
-        for (uint i = 0; i < _count; i++)  
+    // customer interface
+
+    function mint(uint _amount) public payable{
+        require(sale_active, "sale is not active");
+        require(totalSupply() < SELL_MAX, "sold out");        
+        require(totalSupply() + _amount <= SELL_MAX, "not enough tokens for sale");
+        require(0 < _amount <= MULTI_SELL, "number of tokens must be between 1 and 10");
+        require(msg.value >= _amount * PRICE, "not enough money");
+        for (uint i = 0; i < _amount; i++)  
             _safeMint(msg.sender, totalSupply() + 1);
-        if(totalSupply() == SELL_MAX)
-            sale_state = SALE_STATES.FINISHED;
+        emit PurchaseMade(msg.sender, _amount);
     }
 }
