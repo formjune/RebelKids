295fa58afaa628ee1af732f2275ec9445c814349
albert <albert1401@mail.ru>
2021-08-11 15:28:49 +0300

    Deploy famailiars only

--- migrations/2_deploy_familiars.js
+++ migrations/2_deploy_familiars.js
@@ -1,7 +1,5 @@
 const RebelKidsFamiliars = artifacts.require('RebelKidsFamiliars');
-const RebelKids = artifacts.require('RebelKids');
 
 module.exports = function (deployer) {
     deployer.deploy(RebelKidsFamiliars);
-    deployer.deploy(RebelKids);
 };
